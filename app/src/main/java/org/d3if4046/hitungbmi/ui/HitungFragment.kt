b233cfefbc06090768d076613928091a0d573425
Muhammad_Eka_Bakhtiar <bakhtiar@student.telkomuniversity.ac.id>
2021-03-19 00:45:48 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if4046/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if4046/hitungbmi/ui/HitungFragment.kt
@@ -2,12 +2,11 @@ package org.d3if4046.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if4046.hitungbmi.R
 import org.d3if4046.hitungbmi.data.KategoriBmi
 import org.d3if4046.hitungbmi.databinding.FragmentHitungBinding
@@ -23,8 +22,24 @@ class HitungFragment : Fragment() {
             HitungFragmentDirections.actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_about){
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun hitungBmi(){
         val berat = binding.beratEditText.text.toString()
         if (TextUtils.isEmpty(berat)){
@@ -74,4 +89,5 @@ class HitungFragment : Fragment() {
         }
         return getString(stringRes)
     }
+
 }
